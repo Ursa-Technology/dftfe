210435a2e52b06e1b1ebd260d7a7f6b21db5a4c3
Sambit Das <dsambit@umich.edu>
2024-05-09 12:13:01 -0400

    wip

---
+++ include/AuxDensity.h
@@ -0,0 +1,48 @@
+//
+// Created by Sambit Das.
+//
+
+#ifndef DFTFE_AUXDM_AUXDENSITY_H
+#define DFTFE_AUXDM_AUXDENSITY_H
+
+#include <vector>
+#include <utility>
+
+namespace dftfe
+{
+
+  class AuxDensity: public AuxDensityMatrix
+  {
+  public:
+    // Constructor
+    AuxDensityMatrix();
+
+
+    void
+    applyLocalOperations(const std::vector<double> &    Points,
+                         std::map<DensityDescriptorDataAttributes,
+                                  std::vector<double>> &densityData) override;
+
+    void
+    projectDensityMatrix(const std::vector<double> &Qpts,
+                         const std::vector<double> &QWt,
+                         const int                  nQ,
+                         const std::vector<double> &psiFunc,
+                         const std::vector<double> &fValues,
+                         const std::pair<int, int>  nPsi,
+                         double                     alpha,
+                         double                     beta) override;
+
+    void
+    projectDensity(const std::vector<double> &Qpts,
+                   const std::vector<double> &QWt,
+                   const int                  nQ,
+                   const std::vector<double> &densityVals,
+                   const std::vector<double> &gradDensityVals) override;
+
+private:
+
+  };
+} // namespace dftfe
+
+#endif // DFTFE_AUXDM_AUXDENSITY_H
